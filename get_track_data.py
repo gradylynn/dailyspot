7031ed1ebeb07ed0ab498fb953c2d8910d91cc49
Grady Lynn <gradylynn314@gmail.com>
2024-04-16 11:50:05 -0400

    initial attempt at automated schedule updates

--- get_track_data.py
+++ get_track_data.py
@@ -1,11 +1,27 @@
 import os
+import sys
 import time
 import json
+import random
 from datetime import datetime, timedelta
 import requests
 import pandas as pd
 
+playlist_ids = [
+    '37i9dQZEVXcDTxbvihAMNA', # my discover weekly
+    '37i9dQZF1DX0kbJZpiYdZl', # hot hits USA
+    '37i9dQZF1DX1lVhptIYRda', # hot country
+    '37i9dQZF1DXcBWIGoYBM5M', # today's top hits
+    '37i9dQZF1DX0XUsuxWHRQd', # rap caviar
+    '37i9dQZF1EIcwI3ks67O1o', # my mood music mix
+    '37i9dQZF1EIehlJXbuV04P', # my adventure mix
+    '37i9dQZF1DWVqJMsgEN0F4', # alt now
+    '37i9dQZF1DWYV7OOaGhoH0', # roots rising
+]
+
 this_dir = os.path.dirname(os.path.realpath(__file__))
+past_schedule = json.load(open(os.path.join(this_dir, 'schedule.json'), 'r'))
+past_tracks = set(sum([[x['track1Id'], x['track2Id']] for x in past_schedule], []))
 
 token = requests.post(
     'https://accounts.spotify.com/api/token',
@@ -29,9 +45,57 @@ def get_track_info(track_id):
     for album_track in sum((d['tracks'] for d in playcount_resp.json()['data']['discs']), start=[]):
         if track_id in album_track['uri']:
             return album_track
+        
+def get_track_ids(playlist_id):
+    playlist_resp = requests.get(
+        f'https://api.spotify.com/v1/playlists/{playlist_id}',
+        headers={'Authorization': f'Bearer {token["access_token"]}'}
+    )
+    return [
+        i['track']['id']
+        for i in playlist_resp.json()['tracks']['items']
+        if not (i['track']['explicit'] or i['track']['id'] in past_tracks)
+    ]
 
+def update_schedule():
+    track_ids = list(set(sum((get_track_ids(i) for i in playlist_ids), [])))
+    track_ids = sorted(track_ids, key=lambda x: random.random())[:200]
 
-def main():
+    df = pd.DataFrame((get_track_info(id) for id in track_ids))
+    df['track_id'] = df['uri'].apply(lambda x: x.split(':')[-1])
+    df['artists'] = df['artists'].apply(lambda x: ', '.join(sorted([y['uri'] for y in x])))
+    df = df.drop_duplicates(['artists']).sort_values(['playcount'], ascending=False).reset_index(drop=True)
+
+    l1 = list(df[df.index % 2 == 0]['track_id'])
+    l2 = list(df[df.index % 2 == 1]['track_id'])
+    if len(l1) > len(l2):
+        del l1[random.randint(0, len(l1)-1)]
+
+    matchups = []
+    while len(l1) > 0:
+        i = random.randint(0, min(len(l1) - 1, 5))
+        j = random.randint(0, min(len(l2) - 1, 5))
+        matchups.append((l1[i], l2[j]) if random.random() > 0.5 else (l2[j], l1[i]))
+        del l1[i]
+        del l2[j]
+
+    matchups.sort(key=lambda x: random.random())
+
+    d = datetime.date(datetime.strptime(past_schedule[-1]['date'], r'%Y-%m-%d'))
+
+    new_schedule = []
+    for t1id, t2id in matchups:
+        d += timedelta(days=1)
+        new_schedule.append({
+            'date': str(d),
+            'track1Id': t1id,
+            'track2Id': t2id,
+        })
+
+    with open(os.path.join(this_dir, 'schedule.json'), 'w') as f:
+        json.dump(past_schedule + new_schedule, f)
+
+def update_tracks():
     # planning to run this ~12am eastern
     tomorrow = datetime.date(datetime.utcnow())
 
@@ -63,4 +127,5 @@ def main():
         json.dump(window_df.sort_values('date', ascending=False).to_dict('records'), f)
 
 if __name__ == '__main__':
-    main()
+    # just sending it with this mild jank: https://stackoverflow.com/a/52837375
+    globals()[sys.argv[1]]()
